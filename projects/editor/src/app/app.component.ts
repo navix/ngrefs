ef87122aad0fe28366fd82a8992a168a529320d7
Sasha Novyk <inavix@gmail.com>
2018-07-27 15:31:06 +0300

    feat: format content.json

--- projects/editor/src/app/app.component.ts
+++ projects/editor/src/app/app.component.ts
@@ -30,7 +30,7 @@ export class AppComponent {
   }
 
   save() {
-    const blob = new Blob([JSON.stringify(this.data.data)], {type: 'text/plain;charset=utf-8'});
+    const blob = new Blob([JSON.stringify(this.data.data, null, 2)], {type: 'text/plain;charset=utf-8'});
     saveAs(blob, 'content.json');
   }
 }
